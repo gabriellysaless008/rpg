318f73506218fcaa235de3501300a193393147aa
gabriellysaless008 <gabrielly.sales.silva@escola.pr.gov.br>
2026-07-03 14:41:48 -0300

    segunda fase do projeto

--- js/script.js
+++ js/script.js
@@ -1,4 +1,28 @@
-function gerenciarMochila(){
+let item;
+item = document.getElementById( "item" ).value;
 
+function gerenciarMochila(){
+    
+
+    
+    console.log(item);
+    //alert("item adicionado na mochila!");
+
+}
+
+function cadastrar(){
+
+}
+
+function consultar(){
+
+}
+
+function alterar(){
+
+}
+
+function deletar(){
+    document.getElementById("item") = "";
 
 }

9d4d7a3763f163cc9acb565f7efb1b587b8a8b09
gabriellysaless008 <gabrielly.sales.silva@escola.pr.gov.br>
2026-07-06 12:00:30 -0300

    arrumando edentação

--- js/script.js
+++ js/script.js
@@ -5,15 +5,14 @@ function gerenciarMochila(){
     console.log(item);
     //alert("item adicionado na mochila!");
     document.getElementById( "tabela" ).innerHTML = item;
-    
-
-
+    document.getElementById("item").value = "";
 }
 
 function cadastrar(){
     item = document.getElementById("item").value;
     document.getElementById( "tabela" ).innerHTML = item;
-
+    alert("item cadastrado!");
+    document.getElementById("item").value = "";
 }
 
 function consultar(){
@@ -21,25 +20,20 @@ function consultar(){
     busca = prompt("Digite o novo item a ser consultado");
     if (busca == item){
         alert("item encontrado");
-    }else{
-        alert("item não encontrado");
-    }
+        }else{
+            alert("item não encontrado");
+        }
     document.getElementById( "tabela" ).innerHTML = "";
-
 }
 
 function alterar(){
     novo_item = prompt("Digite o novo item a ser alterado");
     document.getElementById( "tabela" ).innerHTML = novo_item;
     document.getElementById("item").value = "";
-
-    
-
 }
 
 function deletar(){
     document.getElementById("item").value = "";
     alert("Item deletado!");
     document.getElementById( "tabela" ).innerHTML = "";
-
 }

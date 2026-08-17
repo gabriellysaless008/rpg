84a95262ef7534afac02399ccb8699daadf6e78a
gabriellysaless008 <gabrielly.sales.silva@escola.pr.gov.br>
2026-08-17 07:43:58 -0300

    arrumando a function consultar()

--- js/script.js
+++ js/script.js
@@ -82,60 +82,53 @@ function mochilaCheia(){
 }
 
 function consultar(){
-    busca = document.getElementById("itemInput").value;
-    tbody = document.getElementById("tabela");
-    
-    if(busca == ""){
-        tbody.innerHTML = tbody.innerHTML + '<tr class="erro"><td colspan="2">Digite um item no campo para consultar!</td></tr>';
-        document.getElementById("itemInput").value = "";
-        setTimeout(function(){
+    let id = Number(document.getElementById("idInput").value);
+    let tbody = document.getElementById("tabela");
+
+    if (id === 0 || isNaN(id)) {
+        tbody.innerHTML += '<tr class="erro"><td colspan="2">Digite um ID para consultar!</td></tr>';
+
+        setTimeout(function() {
+            removerMensagens();
+        }, 3000);
+
+        return;
+    }
+    let encontrado = item.find(function(objeto) {
+        return objeto.id === id;
+    });
+
+    if (!encontrado) {
+        tbody.innerHTML += '<tr class="erro"><td colspan="2">ID "' + id + '" não encontrado!</td></tr>';
+
+        setTimeout(function() {
             removerMensagens();
         }, 3000);
         return;
     }
-    
-    linha = tbody.getElementsByTagName("tr");
-    for(i = 0; i < linha.length; i++){
-        linha[i].className = "";
-    }
-    achei = 0;
-    for(i = 0; i < linha.length; i++){
-        celula = linha[i].getElementsByTagName("td")[0];
-        if(celula.innerHTML == busca){
-            linha[i].className = "destaque";
-            achei = 1;
-            tbody.innerHTML = tbody.innerHTML + '<tr class="mensagem"><td colspan="2">Item "' + busca + '" encontrado e destacado na tabela!</td></tr>';
-            setTimeout(function(){
-                removerMensagens();
-            }, 3000);
+    atualizarTabela();
+    let linhas = tbody.getElementsByTagName("tr");
+    for (let i = 0; i < linhas.length; i++) {
+        let celulas = linhas[i].getElementsByTagName("td");
+
+        if (celulas.length > 0 && celulas[0].innerHTML == encontrado.id) {
+            linhas[i].className = "destaque";
             break;
         }
     }
-    
-    if(achei == 0){
-        tbody.innerHTML = tbody.innerHTML + '<tr class="erro"><td colspan="2">Item "' + busca + '" não encontrado</td></tr>';
-        setTimeout(function(){
-            removerMensagens();
-        }, 3000);
-    }
-    document.getElementById("itemInput").value = "";
-    document.getElementById("idInput").value = "";
-}
 
-function removerMensagens(){
-    tbody = document.getElementById("tabela");
-    linha = tbody.getElementsByTagName("tr");
-    for(i = linha.length - 1; i >= 0; i--){
-        if(linha[i].className == "mensagem" || linha[i].className == "erro"){
-            linha[i].remove();
-        }
-    }
+    tbody.innerHTML += '<tr class="mensagem"><td colspan="2">Item "' + encontrado.item + '" encontrado e destacado na tabela!</td></tr>';
+
+    setTimeout(function() {
+        removerMensagens();
+    }, 3000);
+    document.getElementById("idInput").value = "";
+    document.getElementById("itemInput").value = "";
 }
 function alterar() {
     let id = Number(document.getElementById("idInput").value);
     let novoNome = document.getElementById("itemInput").value;
     let tbody = document.getElementById("tabela");
-
     if (id === 0 || isNaN(id)) {
         tbody.innerHTML += '<tr class="erro"><td colspan="2">Digite um ID para alterar!</td></tr>';
         setTimeout(function() {
@@ -143,7 +136,6 @@ function alterar() {
         }, 3000);
         return;
     }
-
     if (novoNome == "") {
         tbody.innerHTML += '<tr class="erro"><td colspan="2">Digite o novo nome do item!</td></tr>';
         setTimeout(function() {
@@ -151,7 +143,6 @@ function alterar() {
         }, 3000);
         return;
     }
-
     let encontrado = item.find(function(objeto) {
         return objeto.id === id;
     });
@@ -160,8 +151,7 @@ function alterar() {
         setTimeout(function() {
             removerMensagens();
         }, 3000);
-
-        return;
+    return;
     }
 
     let nomeAntigo = encontrado.item;
@@ -206,53 +196,3 @@ function deletar() {
     document.getElementById("idInput").value = "";
     document.getElementById("itemInput").value = "";
 }
-
-// function alterar(){
-//     let id = Number(document.getElementById("idInput").value);
-//     let novoNome = document.getElementById("itemInput").value;
-
-//     let encontrado = item.find(function(objeto) {
-//         return objeto.id === id;
-//     });
-
-//     if (!encontrado) {
-//         alert("ID não encontrado!");
-//         return;
-//     }
-
-//     if (novoNome === "") {
-//         alert("Digite o novo nome do item!");
-//         return;
-//     }
-
-//     encontrado.item = novoNome;
-
-//     atualizarTabela();
-
-//     document.getElementById("idInput").value = "";
-//     document.getElementById("itemInput").value = "";
-
-//     alert("Item alterado com sucesso!");
-// }
-
-
-// function deletar(){
-//     let id = Number(document.getElementById("idInput").value);
-
-//     let indice = item.findIndex(function(objeto) {
-//         return objeto.id === id;
-//     });
-
-//     if (indice === -1) {
-//         alert("ID não encontrado!");
-//         return;
-//     }
-
-//     item.splice(indice, 1);
-
-//     atualizarTabela();
-
-//     document.getElementById("idInput").value = "";
-
-//     alert("Item deletado com sucesso!");
-// }
